2c12f09c7a704496d524ef858adf167f9d56281d
Liutauras Stepanenko <liutauras.stepanenko@gmail.com>
2024-05-03 00:17:58 +0300

    Create table with given name of DB table + functional remove button to remove from database + (non functional) drop down menu with every table name.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_mysqldb import MySQL
+import re
 
 app = Flask(__name__)
 
@@ -11,13 +12,29 @@ app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+mysql = MySQL(app)
+
+
 @app.route('/')
 def main():
     return render_template('index.html')
 
 @app.route('/task_F1')
 def task_F1():
-    return render_template('task_F1.html')
+    table_name = "orders"
+    cur = mysql.connection.cursor()
+    cur.execute(f"SELECT * FROM {table_name}")
+    table = cur.fetchall()
+    for key in table[0].keys():
+        if match:=re.search(r"^(id_.*)$", key):
+            table_id = match.group(1)
+
+    cur.execute('SHOW TABLES')
+    all_tables = cur.fetchall()
+    all_table_names = [list(tab.values())[0] for tab in all_tables]
+    # print(*all_table_names)
+
+    return render_template('task_F1.html', table=table, table_name=table_name, table_id=table_id, all_table_names=all_table_names)
 
 @app.route('/task_F2')
 def task_F2():
@@ -30,6 +47,14 @@ def task_F3():
 @app.route('/task_F4')
 def task_F4():
     return render_template('task_F4.html')
+
+@app.route('/delete_row/<table_name>/<table_id>/<row_id>', methods = ['POST'])
+def delete_row(table_name, table_id, row_id):
+    print(table_name, table_id, row_id)
+    cur = mysql.connection.cursor()
+    cur.execute(f"DELETE FROM {table_name} WHERE {table_id} = {row_id}")
+    mysql.connection.commit()
+    return redirect(request.referrer)
 
 if __name__ == "__main__":
     app.run(debug=True)
